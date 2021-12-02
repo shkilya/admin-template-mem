9cd9b3224a0cff457fd0bb9e766e14e7d6e11abc
shkilniuky <yevhenii.shkilniuk@btcbit.net>
2021-12-02 15:01:32 +0200

    add react admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC} from 'react';
 import './App.css';
+import {Admin, Datagrid, DateField, EditButton, List, Resource, TextField} from "react-admin";
+import jsonServerProvider from 'ra-data-json-server';
+
+const baseURL = 'http://localhost:8030';
+
+const PostList: FC = (props) => {
+    return (
+        <List {...props}>
+            <Datagrid>
+                <TextField source="id"/>
+                <TextField source="title"/>
+                <DateField source="description"/>
+                <EditButton basePath="/posts"/>
+            </Datagrid>
+        </List>
+    )
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Admin dataProvider={jsonServerProvider(baseURL)}>
+            <Resource name="blog" list={PostList}/>
+            <Resource name="test" list={() => {
+                return (<>cxzxzc</>)
+            }}/>
+        </Admin>
+    );
 }
 
 export default App;
